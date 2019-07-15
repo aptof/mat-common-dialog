3607e006ea04f9d533f2a6c878409889ea8ff3d5
Tushar Bakshi <developer.aptof@outlook.com>
2019-07-15 14:14:18 +0530

    second version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatCommonDialogService } from 'mat-common-dialog';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mat-common-dialog-app';
+  constructor(private matSrvice: MatCommonDialogService){}
+
+  notify(){
+    this.matSrvice.notify("You are notified for your fault. Be careful next time", 'Careful', 'Yes').subscribe((resutl)=>console.log(resutl));
+  }
+
+  confirm(){
+    this.matSrvice.confirm("No I am not a hero. Is that right?").subscribe((result)=>console.log(result));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCommonDialogModule } from 'mat-common-dialog';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCommonDialogModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
